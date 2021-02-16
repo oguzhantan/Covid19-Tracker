665169bc799c376573e98cd690afedebd167b04f
Oğuzhan Tan <tannoguzhan97@gmail.com>
2021-02-16 19:18:08 +0200

    My First Commit

---
+++ src/Map.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./Map.css";
+import { showDataOnMap } from './util';
+import { TileLayer, MapContainer as LeafletMap, useMap } from "react-leaflet";
+
+function ChangeMap({ center, zoom }) {
+  const map = useMap();
+  map.setView(center, zoom);
+  return null;
+}
+
+function Map({ countries,casesType,center, zoom }) {
+  return (
+    <div className="map">
+      <LeafletMap>
+        <ChangeMap center={center} zoom={zoom} />
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              ></TileLayer>
+              {showDataOnMap(countries,casesType)}
+      </LeafletMap>
+    </div>
+  );
+}
+
+export default Map
